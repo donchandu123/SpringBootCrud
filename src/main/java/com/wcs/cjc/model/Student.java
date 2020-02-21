98fb044004ecc4978e1e6f5ca093e7b9b33b6a69
donchandu123 <shubhamramtekecjc@gmail.com>
2020-02-21 16:32:17 +0530

    THIS IS MY FIRST COMMIT

---
+++ src/main/java/com/wcs/cjc/model/Student.java
@@ -0,0 +1,35 @@
+package com.wcs.cjc.model;
+
+public class Student {
+
+	
+	private int rollno;
+	private String name;
+	private String areaname;
+	private String cityname;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAreaname() {
+		return areaname;
+	}
+	public void setAreaname(String areaname) {
+		this.areaname = areaname;
+	}
+	public String getCityname() {
+		return cityname;
+	}
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+	
+}
---
+++ src/main/java/com/wcs/cjc/model/Test.java
@@ -0,0 +1,12 @@
+package com.wcs.cjc.model;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Student stu=new Student();
+				stu.setName("SHUBHAM");
+		stu.setRollno(101);
+		stu.setAreaname("WARJE");
+		stu.setCityname("PUNE");
+	}
+}
